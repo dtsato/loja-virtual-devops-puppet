2ecafa31cb7138ecbf1841f5d79296a5e7cc52a8
Danilo Sato <dtsato@gmail.com>
2013-09-30 01:09:06 -0500

    Adicionando manifesto inicial para db, com um arquivo de template

--- db.pp
+++ db.pp
@@ -1,9 +1,25 @@
 exec { "apt-update":
-    command => "/usr/bin/apt-get update"
+  command => "/usr/bin/apt-get update"
 }
-
-Exec["apt-update"] -> Package <| |>
 
 package { "mysql-server":
   ensure => installed,
+  require => Exec["apt-update"],
+}
+
+file { "/etc/mysql/conf.d/allow_external.cnf":
+  owner   => mysql,
+  group   => mysql,
+  mode    => 0644,
+  content => template("/tmp/vagrant-puppet/manifests/allow_ext.cnf"),
+  require => Package["mysql-server"],
+  notify  => Service["mysql"],
+}
+
+service { "mysql":
+  ensure     => running,
+  enable     => true,
+  hasstatus  => true,
+  hasrestart => true,
+  require    => Package["mysql-server"],
 }
